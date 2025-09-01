b727dd3ec88f48aaf27cccdc6ec4e1581708b1c7
sachingujjar4822-lang <sachingujjar4822@gmail.com>
2025-09-01 13:35:37 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Haryana GK Quiz</h1>
+      <p>Welcome to the Haryana GK Quiz App!</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
